f4b218dc6e2cb89c21b16d20221445290143975d
Jonathan <jonathan.sylvestre.pro@gmail.com>
2020-01-14 13:08:56 +0100

    comment added on exo_rb05 file

--- exo_05.rb
+++ exo_05.rb
@@ -1,22 +1,28 @@
-#Affiche un message puts "On va compter le nombre d'heures de travail à THP"
- puts "Travail : #{10 * 5 * 11}"
+#Affiche une chaine de caracteres
+puts "On va compter le nombre d'heures de travail à THP"
+#renvoie 550
+puts "Travail : #{10 * 5 * 11}"
+#renvoie 33000
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
-
+#renvoie une chaine de caracteres
 puts "Et en secondes ?"
-
+#renvoie 1980000
 puts 10 * 5 * 11 * 60 * 60
-
+#renvoie une chaine de caracteres
 puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
-
+#renvoie false
 puts 3 + 2 < 5 - 7
-
+#renvoie 5
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#renvoie -2
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
-
+#renvoie une chaine de caracteres
 puts "Ok, c'est faux alors !"
-
+#renvoie une chaine de caracteres
 puts "C'est drôle ça, faisons-en plus :"
-
+#renvoie true 
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#renvoie true
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#renvoie false
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
